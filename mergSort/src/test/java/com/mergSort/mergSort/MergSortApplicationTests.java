2ceabbcb56bbb84b621300d52af88858b1b745c8
Shikha Gaur <shikha.gaur@sugarboxnetworks.com>
2020-04-26 16:33:56 +0530

    merge sort

---
+++ mergSort/src/test/java/com/mergSort/mergSort/MergSortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mergSort.mergSort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergSortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
